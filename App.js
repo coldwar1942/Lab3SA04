40c0eb6805ab3ac711801cddcc56247197d3b59e
coldwar1942 <chakrit.jira@gmail.com>
2023-08-15 20:32:56 +0700

    Router

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import Weather from './Components/Weather.js'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './Components/ZipCodeScreen.js';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-    <Weather zipCode="90110"/>
-    <StatusBar style="auto" />
-    </View>
-  );
+  return ( 
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="Home" component={ZipCodeScreen} />
+          </Stack.Navigator>
+      </NavigationContainer> 
+  )
 }
 
 const styles = StyleSheet.create({
